284fe1b51ae55399b6d939b12ffd93a6e4ba8160
kaineop <52883126+kaineop@users.noreply.github.com>
2019-07-15 21:09:26 +0200

    Update main.ino

--- arduino/main.ino
+++ arduino/main.ino
@@ -2,21 +2,35 @@ void setup() {
   //  copyright 
 
   Serial.begin(9600);
+  
   Serial.println("1Started Arduino");
 
 }
 
 void loop() {
 
-  if (Serial.readString() == "print-done") {
-   // Exeute motors etc 
 
+ while(Serial.available() > 0 ){
+    String str = Serial.readString();
+    if(str.indexOf("printdone") > -1){
+      Serial.println("G1 Y5 F250");
+   // Exeute motors etc 
+       
    delay(1000);
 
    Serial.println("1select-print");
 
    delay (2000);
    Serial.println("1start-print");
+
+    } else {
+      Serial.println("unknown");
+    }
+  }
+
+
+    
+   
 
    
    
@@ -25,4 +39,3 @@ void loop() {
   }
 
 
-}
